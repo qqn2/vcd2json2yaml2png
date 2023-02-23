2422c5ee9528c4241871c88ebfd253fd9325c5ef
Samy Rekioua <rek@melexis.com>
2023-02-23 17:26:50 +0100

    Fix incorrect data formatting for data field

--- vcd2yaml.py
+++ vcd2yaml.py
@@ -28,15 +28,15 @@ def traverse_dict(d, indent=0):
         if isinstance(value, dict):
             yaml_data += " " * indent + f"{key}:\n"
             yaml_data += traverse_dict(value, indent + 2)
+        elif isinstance(value, str) and key == "data":
+            data_values = value.split()
+            yaml_data += " " * indent + f"{key}:\n"
+            for data_value in data_values:
+                yaml_data += " " * (indent + 2) + f"- {data_value}\n"
         else:
-            # Add two spaces before the "wave" field
-            if key == "wave":
-                yaml_data += " " * (indent + 2) + f"{key}: {value}\n"
-            elif key == "data":
-                yaml_data += " " * (indent + 2) + f"{key}: {value}\n"
-            else:
-                yaml_data += " " * indent + f"{key}: {value}\n"
+            yaml_data += " " * indent + f"{key}: {value}\n"
     return yaml_data
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -46,6 +46,7 @@ if __name__ == "__main__":
     filename = sys.argv[1]
 
     extractor = WaveExtractor(filename, f"{filename.split('.')[0]}.json", [])
+    extractor.wave_chunk = 30000
     extractor.execute()
 
     with open(f"{filename.split('.')[0]}.json") as f:
